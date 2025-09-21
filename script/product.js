386e185353e409c3fda20a3351dbef16cd53b409
Brenda Anastasya <brenda.535240097@stu.untar.ac.id>
2025-09-21 15:02:48 +0700

    Delete script directory

--- script/product.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const buttons = document.querySelectorAll(".product-card .btn");
-
-  buttons.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      alert("More product details will be available soon!");
-    });
-  });
-});
